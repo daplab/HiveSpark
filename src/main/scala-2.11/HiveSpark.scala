1605eaec7ca2316c809b9dcb80f0434c5d571940
Benoit Perroud <benoit@sqooba.io>
2019-10-31 06:52:04 +0100

    Small updates

--- src/main/scala-2.11/HiveSpark.scala
+++ src/main/scala-2.11/HiveSpark.scala
@@ -2,23 +2,14 @@
  * Created by cbovigny on 10/23/15.
  */
 
-
-/**
- * Created by cbovigny on 10/22/15.
- */
-
-import com.google.common.io.{Files, ByteStreams}
-
-import java.io.File
-
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql._
 import org.apache.spark.sql.hive.HiveContext
 
-
+/* Run me:
+ * spark-submit --class "HiveSpark" --master yarn-client hivespark_2.11-1.0.jar
+ */
 object HiveSpark {
-
-
 
   def main(args: Array[String]) {
     val sparkConf = new SparkConf().setAppName("HiveFromSpark")
@@ -28,22 +19,19 @@ object HiveSpark {
     // using HiveQL. Users who do not have an existing Hive deployment can still create a
     // HiveContext. When not configured by the hive-site.xml, the context automatically
     // creates metastore_db and warehouse in the current directory.
+
     val hiveContext = new HiveContext(sc)
-    val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
     import hiveContext.implicits._
     import hiveContext.sql
 
-
+    // 1. Get the count number of BXDataset
     val count = sql("SELECT COUNT(*) FROM BXDataSet").collect().head.getLong(0)
     println(s"COUNT(*): $count")
-    // Here you have to complete the query :
-    val YearCount=hiveContext.sql("from bxdataset SELECT yearofpublication, count(booktitle) group by yearofpublication order by yearofpublication").collect().foreach(println)
 
+    // 2. Here you have to complete the query :
+    val yearCount = sql("from bxdataset SELECT yearofpublication, count(booktitle) group by yearofpublication order by yearofpublication").collect().foreach(println)
 
 
     sc.stop()
   }
-
-
 }
-
